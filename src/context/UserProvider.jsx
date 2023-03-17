18446b6775b340582c386ac3c871e1ec434ebbab
Arma Burton <arma@armaburton.com>
2023-03-16 21:03:11 -0700

    userprovider

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -7,4 +7,60 @@ import {
   useState,
 } from "react";
 import { getCurrentUser, signIn, signOut } from "../services/users";
-import 
+import { renderView } from "../utils/renderView";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const login = async ({ username, password }) => {
+    try {
+      const user = await signIn(username, password);
+      setUser(user);
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  const logout = useCallback(() => {
+    signOut().then(() => setUser(null));
+  });
+
+  useEffect(() => {
+    getCurrentUser()
+      .then(setUser)
+      .finally(() => setLoading(false));
+  }, []);
+
+  const state = useMemo(
+    () => ({ loading, user, logout, login }),
+    [loading, user, logout, login]
+  );
+
+  return (
+    <UserContext.Provider value={state}>
+      {renderView({ ...state, children })}
+    </UserContext.Provider>
+  );
+};
+
+export const useCurrentUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined)
+    throw new Error("useCurrentUser must be used within a UserProvider");
+
+  return context.user;
+};
+
+export const useAuth = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined)
+    throw new Error("useAuth must be used within a UserProvider");
+
+  return { logout: context.logout, login: context.login };
+};
