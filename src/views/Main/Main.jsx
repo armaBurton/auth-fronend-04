6e65856dd4accce17d1da307c8c2d3c8e1f81512
Arma Burton <arma@armaburton.com>
2023-03-20 18:52:32 -0700

    working on main a little bit

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -2,6 +2,10 @@ import { useCurrentUser } from "../../context/UserProvider";
 
 export const Main = () => {
   const user = useCurrentUser();
-
-  return <>{user.username}</>;
+  console.log(`*** JSON.stringify(user) ==> ` + JSON.stringify(user));
+  return (
+    <section className="main-section">
+      {user.status === 401 ? user.message : user.username}
+    </section>
+  );
 };
