e48368645e70a2a1fbc3b655c7a15d8ab8118d09
Arma Burton <arma@armaburton.com>
2023-03-23 16:50:00 -0700

    testing outlet in privateRoutes

--- src/components/PrivateRoutes/PrivateRoutes.jsx
+++ src/components/PrivateRoutes/PrivateRoutes.jsx
@@ -3,7 +3,13 @@ import { useCurrentUser } from "../../context/UserProvider";
 import { getCurrentUser } from "../../services/users";
 
 export const PrivateRoutes = async () => {
-  const user = await getCurrentUser();
+  const user = await useCurrentUser();
 
-  return user.username ? <Outlet /> : <Navigate to="/main" />;
+  return (
+    <div>
+      <h1>testing Outlet</h1>
+      <Outlet />
+    </div>
+  );
+  // return user.username ? <Outlet /> : <Navigate to="/main" />;
 };
