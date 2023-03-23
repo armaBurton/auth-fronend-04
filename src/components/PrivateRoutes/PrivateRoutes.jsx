f2024dc8f9c88da977b6b6cb7c3861c013ba21c2
Arma Burton <arma@armaburton.com>
2023-03-22 20:48:52 -0700

    i still don't know

--- src/components/PrivateRoutes/PrivateRoutes.jsx
+++ src/components/PrivateRoutes/PrivateRoutes.jsx
@@ -1,8 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useCurrentUser } from "../../context/UserProvider";
+import { getCurrentUser } from "../../services/users";
 
 export const PrivateRoutes = async () => {
-  const user = await useCurrentUser();
+  const user = await getCurrentUser();
 
-  return user?.username ? <Outlet /> : <Navigate to="/main" />;
+  return user.username ? <Outlet /> : <Navigate to="/main" />;
 };
