5449561e2153517b06ec97d0e2a82c0d9adc3c8e
Arma Burton <arma@armaburton.com>
2023-03-23 17:09:33 -0700

    still not working

--- src/components/PrivateRoutes/PrivateRoutes.jsx
+++ src/components/PrivateRoutes/PrivateRoutes.jsx
@@ -1,15 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useCurrentUser } from "../../context/UserProvider";
-// import { getCurrentUser } from "../../services/users";
 
 export const PrivateRoutes = async () => {
-  try {
-    const user = await useCurrentUser();
-    console.log(`*** user ==> ` + user);
-  } catch (err) {
-    console.error(err);
-  }
-  return <Outlet />;
+  const user = await useCurrentUser();
+  console.log(`*** user ==> ` + user);
 
-  // return user.username ? <Outlet /> : <Navigate to="/main" />;
+  return user.username ? <Outlet /> : <Navigate to="/main" />;
 };
