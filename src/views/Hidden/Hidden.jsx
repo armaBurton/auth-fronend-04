a8edd2033634c1a0ce06f76d3c41f4ca05c1fc48
Arma Burton <arma@armaburton.com>
2023-03-23 21:11:05 -0700

    styles

--- src/views/Hidden/Hidden.jsx
+++ src/views/Hidden/Hidden.jsx
@@ -5,9 +5,9 @@ export const Hidden = () => {
   const user = useCurrentUser();
   return (
     <section className="hidden-section">
-      <h1>
+      <p>
         welcome_to_the_hidden_page_<span>{user.username}</span>
-      </h1>
+      </p>
     </section>
   );
 };
