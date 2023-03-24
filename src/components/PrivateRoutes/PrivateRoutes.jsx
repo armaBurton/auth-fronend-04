0f14253ef22acf4d92a2f5a93e7df81468250ab4
Arma Burton <arma@armaburton.com>
2023-03-23 20:06:34 -0700

    cleanup

--- src/components/PrivateRoutes/PrivateRoutes.jsx
+++ src/components/PrivateRoutes/PrivateRoutes.jsx
@@ -3,7 +3,6 @@ import { useCurrentUser } from "../../context/UserProvider";
 
 export const PrivateRoutes = () => {
   const user = useCurrentUser();
-  console.log(`*** user ==> ` + user);
 
   return user.username ? <Outlet /> : <Navigate to="/main" />;
 };
