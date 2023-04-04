d4063db348f922d55bd83861174e50e091675dcc
Arma Burton <arma@armaburton.com>
2023-04-04 13:36:01 -0700

    post display, styling

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import "./Posts.css";
 
 export default function Posts({ posts, currentSelected }) {
   const focused = useRef();
@@ -14,11 +15,12 @@ export default function Posts({ posts, currentSelected }) {
   return (
     <ul>
       {posts.map((post) => (
-        <li key={post.id}>
+        <li key={post.id} className="list-element">
           <Link
+            className="link-decoration"
             to={`/posts/${post.id} {...{ref: currentSelected=== post.id ? focused: null}}`}
           >
-            {post.title}
+            <p className="post-title">{post.title}</p>
           </Link>
         </li>
       ))}
