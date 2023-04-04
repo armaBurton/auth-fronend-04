9e4c25434875082d1db948ab8f1d414b21104eff
Arma Burton <arma@armaburton.com>
2023-04-04 13:03:42 -0700

    PostsProvider useEffect()

--- src/context/PostProvider.jsx
+++ src/context/PostProvider.jsx
@@ -1,3 +1,18 @@
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { getPosts } from "../services/posts";
 import { renderView } from "../utils/renderView";
+
+const PostsContext = createContext();
+
+export const PostsProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts()
+      .then(setPosts)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }, []);
+};
